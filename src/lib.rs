9434ad396d76488411662ad107ca12833ec7bbd5
Phoebe Goldman <phoebe@goldman-tribe.org>
2020-12-20 19:10:21 -0600

    a couple comments

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,23 @@
 use std::panic::{AssertUnwindSafe, catch_unwind, resume_unwind};
 use std::sync::atomic::{AtomicU64, Ordering};
 
-static NEXT_TOKEN: AtomicU64 = AtomicU64::new(0);
-
 #[derive(PartialEq, Eq, Copy, Clone, Hash, Debug)]
+/// A globally-unique thing with no properties other than total
+/// equality.
+///
+/// To construct a continuation, we create a unique `Token`. The
+/// continuation invokes an unwind with that token as its payload. The
+/// resume point catches unwinds and compares their payload to its
+/// token. If they are the same, it takes an action appropriate to the
+/// continuation; if not, it re-throws the unwind.
 struct Token(u64);
 
 impl Token {
     fn next() -> Self {
+        // Range of `u64` is large enough that wrapping is not a plausible
+        // issue, so this counter will be monotonically increasing.
+        static NEXT_TOKEN: AtomicU64 = AtomicU64::new(0);
+
         let token = NEXT_TOKEN.fetch_add(1, Ordering::Relaxed);
         Token(token)
     }
@@ -52,25 +62,44 @@ where
     Body: FnMut(Payload, &mut dyn FnMut(Payload) -> !) -> Return,
 {
     let my_token = Token::next();
+    // Ideally, this `Option` should be optimized away, since our
+    // accesses to it strictly follow a loop of first initializing or
+    // `replace`ing it, then `take`ing it. It would be easy to
+    // manually remove the `Option` using `unsafe` with `MaybeUninit`,
+    // but that would be kinda gross...
     let mut val = Some(initial_payload);
     'repeat: loop {
-        match catch_unwind(AssertUnwindSafe(|| {
-            let payload = val.take().unwrap();
-            body(
-                payload,
-                &mut |new_payload| {
-                    val.replace(new_payload);
-                    resume_unwind(Box::new(my_token))
-                }
-            )
-        })) {
+        match catch_unwind(
+            // Because we're only catching our own unwinds, it
+            // shouldn't be any more possible to break invariants than
+            // normal, unless a buggy consumer invokes a repeat
+            // continuation at a time when its invariants do not
+            // hold. That would be their problem, not ours.
+            AssertUnwindSafe(|| {
+                let payload = val.take().unwrap();
+                body(
+                    payload,
+                    &mut |new_payload| {
+                        val.replace(new_payload);
+                        // Use `resume_unwind` rather than `panic!` to
+                        // avoid triggering the panic hook, since this
+                        // isn't an actual panic.
+                        resume_unwind(Box::new(my_token))
+                    }
+                )
+            })) {
             Ok(ret) => return ret,
             Err(thrown_token) => {
                 if let Some(&thrown_token) = thrown_token.downcast_ref::<Token>() {
                     if thrown_token == my_token {
+                        // If this is our unwind, repeat the loop.
+                        // note that the continuation will have
+                        // already replaced `val` with a new payload.
                         continue 'repeat
                     }
                 }
+                // If we've caught someone else's unwind, bubble it up
+                // the stack
                 resume_unwind(thrown_token)
             }
         }
@@ -124,14 +153,22 @@ where
     Body: FnOnce(&mut dyn FnMut(E) -> !) -> T,
 {
     let mut body = Some(body);
+    // There's likely a more efficient implementation not in terms of
+    // `call_with_repeat_continuation`, but I don't feel like writing
+    // it...
     call_with_repeat_continuation(
         None,
         move |error, throw| {
             if let Some(err) = error {
+                // Second time through, after invoking the escape
+                // continuation. Return the error.
                 Err(err)
             } else if let Some(body) = body.take() {
+                // First time through, with `error == initial_payload
+                // == None`. Pass an escape continuation to `body`.
                 Ok(body(&mut |err| throw(Some(err))))
             } else {
+                // Third or more time through. Should be impossible.
                 unreachable!("Loop in call/ec")
             }
         }
